0175ef6aa272f75b2b4c2fe7a2d35bd304320013
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2025-12-21 22:39:57 +0530

    done

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
+import AdminDashboard from './components/Dashboard/AdminDashboard'
 
 const App = () => {
   return (
@@ -8,8 +9,10 @@ const App = () => {
     
     < >
 
-    {/*<Login />*/}
-    <EmployeeDashboard />
+    <Login />
+   {/*<EmployeeDashboard /> */}
+
+   {/*<AdminDashboard />*/}
     </>
     </div>
   )
